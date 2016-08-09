7607c11266dbc2e9828c07d59931521cb1059183
PriyankaUma <priyankauma96@gmail.com>
2016-08-09 18:41:11 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import.java.io*;
+class loop
+{
+public static void main(String[] args);
+{
+Scanner s=new Scanner(System.in);
+String a=s.next();
+for(int i=0;i<5;i++)
+{
+System.out.println("hello");
+
+}
+}
+}
